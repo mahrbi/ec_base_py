8a1ada5281075588e3670a4f0520f31fbb106530
icdl-webmaster21 <87164460+icdl-webmaster21@users.noreply.github.com>
2021-12-01 13:52:34 +0900

    cc

--- ec_base/urls.py
+++ ec_base/urls.py
@@ -18,6 +18,9 @@ from django.urls import path, include
 import debug_toolbar
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+from django.conf.urls import url
+
 admin.site.site_header = 'ec_base Admin'
 admin.site.index_title = 'Admin'
 
@@ -28,6 +31,8 @@ urlpatterns = [
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
     path('__debug__/', include(debug_toolbar.urls)),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
